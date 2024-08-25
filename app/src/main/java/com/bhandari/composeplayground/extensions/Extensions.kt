d4a1cdda69a3a5e6f013e8bd1408b225c527450f
bob <amit.bhandari.c@gmail.com>
2024-08-25 21:07:58 +0530

    Added timer and made stopwatch work

--- app/src/main/java/com/bhandari/composeplayground/extensions/Extensions.kt
+++ app/src/main/java/com/bhandari/composeplayground/extensions/Extensions.kt
@@ -17,9 +17,15 @@ fun Int.isDivisible(divisor: Int): Boolean {
 
 @SuppressLint("DefaultLocale")
 fun Long.toHms(): String {
-    val hours = this / 3600000
-    val minutes = (this % 3600000) / 60000
+    val minutes = this / 60000
     val seconds = (this % 60000) / 1000
+    val milliseconds = (this % 1000) / 10
 
-    return String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    return String.format("%02d:%02d:%02d", minutes, seconds, milliseconds)
+}
+
+fun Long.toRange0To360(): Float {
+    val out = this/1000f * (360f / 60f)
+    println("toRange0To360: $this: $out")
+    return out
 }
